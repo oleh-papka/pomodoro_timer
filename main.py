ec497382645169a3b2f6432102ff933d01db7927
Oleh Papka <olegpapka2@gmail.com>
2024-03-22 14:04:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-""" Simple pomadoro timer for Linux.
+""" Simple pomodoro timer for Linux.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
